dcfbb54855259879d1f4a9f2790b49bb3c216a5e
Tyler Norman <tylern@ravebizz.com>
2022-06-24 21:06:36 -0400

    Replaced custom search with built in app bar and added nav view menu with no functionality for options

--- app/src/main/java/com/streamability/alexisdaddi/ui/search/SearchFragment.kt
+++ app/src/main/java/com/streamability/alexisdaddi/ui/search/SearchFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.core.view.children
 import androidx.fragment.app.viewModels
+import com.streamability.alexisdaddi.MainActivity
 import com.streamability.alexisdaddi.R
 import com.streamability.alexisdaddi.databinding.SearchFragmentBinding
 import com.streamability.alexisdaddi.ui.search.SearchViewModel
@@ -25,6 +29,14 @@ class SearchFragment : Fragment() {
     ) = SearchFragmentBinding.inflate(inflater, container, false).also {
         _binding = it
     }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
+        super.onViewCreated(view, savedInstanceState)
+
+        options.topAppBar.setNavigationOnClickListener{
+            options.drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
 
     override fun onDestroy() {
         super.onDestroy()
