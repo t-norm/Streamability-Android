163b2d2194fe310821574521241cd9647ae6f73b
Tyler Norman <tylern@ravebizz.com>
2022-06-25 19:02:55 -0400

    Basic dark mode, more work needed

--- app/src/main/java/com/streamability/alexisdaddi/ui/search/SearchFragment.kt
+++ app/src/main/java/com/streamability/alexisdaddi/ui/search/SearchFragment.kt
@@ -1,19 +1,17 @@
 package com.streamability.alexisdaddi.ui.search
 
-import androidx.lifecycle.ViewModelProvider
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.GravityCompat
-import androidx.core.view.children
 import androidx.fragment.app.viewModels
-import com.streamability.alexisdaddi.MainActivity
 import com.streamability.alexisdaddi.R
 import com.streamability.alexisdaddi.databinding.SearchFragmentBinding
-import com.streamability.alexisdaddi.ui.search.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -33,9 +31,51 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
 
-        options.topAppBar.setNavigationOnClickListener{
+        val topAppBar = options.topAppBar
+        val menu = options.navView
+
+        if(AppCompatDelegate.getDefaultNightMode()== AppCompatDelegate.MODE_NIGHT_YES)
+        {
+            getMenuItem(R.id.darkmode_item).setIcon(R.drawable.ic_baseline_light_mode_24)
+            getMenuItem(R.id.darkmode_item).title = "Light Mode"
+        } else {
+            getMenuItem(R.id.darkmode_item).setIcon(R.drawable.ic_baseline_dark_mode_24)
+            getMenuItem(R.id.darkmode_item).title = "Dark Mode"
+        }
+
+
+        topAppBar.setNavigationOnClickListener{
             options.drawerLayout.openDrawer(GravityCompat.START)
         }
+
+        menu.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.darkmode_item -> {
+                    if(AppCompatDelegate.getDefaultNightMode()== AppCompatDelegate.MODE_NIGHT_YES)
+                    {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                    true
+                }
+                R.id.region_item -> {
+                    // code here
+                    true
+                }
+                R.id.logout_item -> {
+                    // code here
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    private fun getMenuItem(menuItem: Int): MenuItem = with(binding) {
+        return options.navView.menu.findItem(menuItem)
     }
 
     override fun onDestroy() {
